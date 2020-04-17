85982da42fd453a6b7289fc312bab59a16896b57
fany-etc <extrepa@hotmail.com>
2020-04-17 18:17:06 -0500

    final

--- script/script.js
+++ script/script.js
@@ -1,3 +1,13 @@
-$('#myCarousel').on('slide.bs.carousel', function () {
-    // do something...
-  })
+var unete = document.getElementById('unete')
+var usuario = document.getElementById('usuario')
+var ingreso = document.getElementById('ingreso')
+
+ingreso.addEventListener('click',function(){
+  if (usuario.classList[1]=="d-none"){
+    usuario.classList.remove('d-none')
+    unete.classList.add('d-none')
+  }else if (unete.classList[1]=="d-none"){
+    unete.classList.remove('d-none')
+    usuario.classList.add('d-none')
+  }
+})
